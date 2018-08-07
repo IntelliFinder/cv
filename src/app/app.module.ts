79ba0a08074d9c4b94504fc2531d6aba2c4af6ca
IntelliFinder <snirhordan@gmail.com>
2018-08-07 21:04:46 +0300

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ExpComponent } from './exp/exp.component';
+import { EduComponent } from './edu/edu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpComponent,
+    EduComponent
   ],
   imports: [
     BrowserModule
