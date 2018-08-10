024f3ad7f4a8b4fa32a3f86b157d2f557157a2c6
IntelliFinder <snirhordan@gmail.com>
2018-08-10 03:36:58 +0300

    crude display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { DashboardComponent }   from './dashboard/dashboard.component';
 import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 import { HeroesComponent }      from './heroes/heroes.component';
 import { MessagesComponent }    from './messages/messages.component';
-
-import { AppRoutingModule }     from './app-routing.module';
 import { ItemComponent } from './item/item.component';
 import { ItemDetailComponent } from './item-detail/item-detail.component';
+
+import { AppRoutingModule }     from './app-routing.module';
+
 
 @NgModule({
   imports: [
